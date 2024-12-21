fb86ae50729157a378341e0869eea2a42b2aa62f
lorevlmt <lore.vimontes@gmail.com>
2024-12-21 15:08:38 +0100

    Ejercicio 20 resuelto

--- src/main/java/com/campusdual/classroom/Exercise20.java
+++ src/main/java/com/campusdual/classroom/Exercise20.java
@@ -6,11 +6,20 @@ import java.util.List;
 public class Exercise20 {
 
     public static List<Person> getPeopleList() {
+        List<Person> stringList = new ArrayList<>();
 
+        stringList.add(new Person("John", "Smith"));
+        stringList.add(new Teacher("María", "Montessori", "Educación"));
+        stringList.add(new PoliceOfficer("Jake", "Peralta", "B-99"));
+        stringList.add(new Doctor("Gregory", "House", "Nefrología e infectología"));
+
+        return stringList;
     }
 
     public static void showPeopleDetails(List<Person> stringList) {
-
+        for (Person s: stringList) {
+            s.getDetails();
+        }
     }
 
     public static void main(String[] args) {
